9af36652ecb8e18b1d634838aa3cb7483ef340ab
voluntariadoohisergio <voluntariadoohisergio>
2018-06-08 16:54:32 -0500

    informacion de captura de carbono en manglares

---
+++ prep/CS/cs_manglar_extencion_2018.r
@@ -0,0 +1,8 @@
+---
+title: "cs_manglar_extencion_2018"
+output: html_document
+---
+
+*fuente*:Valderrama-Landeros, L. H., Rodríguez-Zúñiga, M. T., Troche-Souza, C., Velázquez-Salazar, S., Villeda-Chávez, E., Alcántara-Maya, J. A., ... & Ressl, R. (2017). Manglares de México: actualización y exploración de los datos del sistema de monitoreo 1970/1980–2015. Comisión Nacional para el Conocimiento y Uso de la Biodiversidad. Ciudad de México, 128pp Google Scholar.						
+Herrera Silveira, J. A., Camacho Rico, A., Pech, E., Pech, M., Ramírez Ramírez, J., & Teutli Hernández, C. (2016). Dinámica del carbono (almacenes y flujos) en manglares de México. Terra Latinoamericana, 34(1), 61-72.						
+Luis Humberto Valderrama Landeros, et al (2015) informe CONABIO, ACTUALIZACIÓN Y EXPLORACIÓNDE LOS DATOS DEL SISTEMA DE MONITOREO de manglares de Mexico. P 23-25. 						
