d8e9df3c1609ade047431851e493b56e673861b7
voluntariadoohisergio <voluntariadoohisergio>
2018-06-15 15:01:27 -0500

    informacion de cp por manglares

---
+++ prep/CP/cp_manglar_area_2018.r
@@ -0,0 +1,9 @@
+---
+title: "cp_manglares_area_2018"
+output: html_document
+---
+
+*fuente*Valderrama-Landeros L. H., Rodríguez-Zúñiga M.T., Troche-Souza C., Velázquez-Salazar, S., Villeda-Chávez,E., Alcántara-Maya, J.A., Vázquez-Balderas B., Cruz-López M. I., Ressl R., 2017. Manglares de México: actualización,y exploración de los datos del sistema de monitoreo 1970/1980–2015. Comisión Nacional para el Conocimiento y Uso de la Biodiversidad. Ciudad de México, 128 pp.			
+			
+			
+			
