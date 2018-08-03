70f938aaa85468b9b7598712c31f9f6385527763
voluntariadoohisergio <voluntariadoohisergio>
2018-08-03 17:01:34 -0500

    información de arrecifes coralinos para proteccion costera

---
+++ prep/CP/cp_arrecifes_area_2018.r
@@ -0,0 +1,7 @@
+---
+title: "cp_arrecifes_area_2018"
+output: html_document
+---
+
+*fuente*INEGI, 2015. Catálogo de Territorio Insular Mexicano
+Juárez-Hernandez, Tapia-García, 2017. Varciación espacial en números de especies, abundancia y diversidad de peces en las bahías de Huatulco, Oaxaca, México.			
