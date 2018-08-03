0484468fbdee4c72eb6ee4f7d8b66aa6920e464f
voluntariadoohisergio <voluntariadoohisergio>
2018-08-03 17:33:46 -0500

    informacion de humedales para proteccion costera

---
+++ prep/CP/cp_humedal_area_2018.r
@@ -0,0 +1,6 @@
+---
+title: "cp_humedal_area_2018"
+output: html_document
+---
+
+*fuente*Landgrave, R., & Moreno-Casasola, P. (2012). Evaluación cuantitativa de la pérdida de humedales en México. Investigación ambiental Ciencia y política pública, 4(1).			
